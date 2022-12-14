bdcb323c54a2311c9c3638f350a6e2478cfaa1e0
maxporyvay <sevmaxclever@gmail.com>
2022-12-15 01:56:39 +0300

    Programs from 08.12 hometask made

---
+++ hometask08_12/router.rb
@@ -0,0 +1,38 @@
+require_relative "posts_controller"
+
+class Router
+    def initialize
+        @routes = {}
+    end
+  
+    def init
+        resources(PostsController, 'posts')
+  
+        loop do
+            print 'Choose resource you want to interact (1 - Posts, 2 - Comments, q - Exit): '
+            choise = gets.chomp
+  
+            PostsController.connection(@routes['posts']) if choise == '1'
+            break if choise == 'q'
+        end
+  
+        puts 'Good bye!'
+    end
+  
+    def resources(klass, keyword)
+        controller = klass.new
+        @routes[keyword] = {
+            'GET' => {
+                'index' => controller.method(:index),
+                'show' => controller.method(:show)
+            },
+            'POST' => controller.method(:create),
+            'PUT' => controller.method(:update),
+            'DELETE' => controller.method(:destroy)
+        }
+    end
+end
+  
+router = Router.new
+  
+router.init
