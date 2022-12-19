8a4a37a2148a5ab28a03121ed1e4b4592978ff19
maxporyvay <sevmaxclever@gmail.com>
2022-12-20 01:27:08 +0300

    Minor indent changes in 13.12 hometask

--- hometask13_12/cashmachine.rb
+++ hometask13_12/cashmachine.rb
@@ -4,67 +4,67 @@ DEFAULT_START_BALANCE = 100.0
 BALANCE_FILE = "balance.txt"
 
 class CashMachine
-    def self.init(balance_file, default_start_balance)
-        if File.exists?(balance_file)
-            balance = find(balance_file, 0).to_f
-        else
-            balance = default_start_balance
-        end
-
-        cashMachine = CashMachine.new(balance)
-
-        loop do
-            print "Enter command>"
-            command = gets.chomp.downcase
-            case command
-            when "d"
-                print "Enter deposit amount>"
-                deposit_amount = gets.chomp.to_f
-                puts cashMachine.make_deposit(deposit_amount)
-            when "w"
-                print "Enter withdraw amount>"
-                withdraw_amount = gets.chomp.to_f
-                puts cashMachine.make_withdraw(withdraw_amount)
-            when "b"
-                puts cashMachine.check_balance
-            when "q"
-                cashMachine.save_balance(balance_file)
-                break
-            else
-                puts "ERROR! Invalid command. Press d/w/b/q"
-            end
-        end
+  def self.init(balance_file, default_start_balance)
+    if File.exists?(balance_file)
+      balance = find(balance_file, 0).to_f
+    else
+      balance = default_start_balance
     end
 
-    def initialize(balance)
-        @balance = balance
-    end
+    cashMachine = CashMachine.new(balance)
 
-    def make_deposit(amount)
-        if amount <= 0
-            return "ERROR! Deposit amount must be greater than 0"
-        else
-            @balance += amount
-            return "Your balance is now #{@balance.round(2)}"
-        end
+    loop do
+      print "Enter command>"
+      command = gets.chomp.downcase
+      case command
+      when "d"
+        print "Enter deposit amount>"
+        deposit_amount = gets.chomp.to_f
+        puts cashMachine.make_deposit(deposit_amount)
+      when "w"
+        print "Enter withdraw amount>"
+        withdraw_amount = gets.chomp.to_f
+        puts cashMachine.make_withdraw(withdraw_amount)
+      when "b"
+        puts cashMachine.check_balance
+      when "q"
+        cashMachine.save_balance(balance_file)
+        break
+      else
+        puts "ERROR! Invalid command. Press d/w/b/q"
+      end
     end
+  end
 
-    def make_withdraw(amount)
-        if amount <= 0 || amount > @balance
-            return "ERROR! Withdraw amount must be greater than 0 and less or equal than your balance"
-        else
-            @balance -= amount
-            return "Your balance is now #{@balance.round(2)}"
-        end
-    end
+  def initialize(balance)
+    @balance = balance
+  end
 
-    def check_balance
-        return "Your balance is #{@balance.round(2)}"
+  def make_deposit(amount)
+    if amount <= 0
+      return "ERROR! Deposit amount must be greater than 0"
+    else
+      @balance += amount
+      return "Your balance is now #{@balance.round(2)}"
     end
+  end
 
-    def save_balance(balance_file)
-        File.open(balance_file, 'w') {|f| f.write(@balance.round(2))}
+  def make_withdraw(amount)
+    if amount <= 0 || amount > @balance
+      return "ERROR! Withdraw amount must be greater than 0 and less or equal than your balance"
+    else
+      @balance -= amount
+      return "Your balance is now #{@balance.round(2)}"
     end
+  end
+
+  def check_balance
+    return "Your balance is #{@balance.round(2)}"
+  end
+
+  def save_balance(balance_file)
+    File.open(balance_file, 'w') {|f| f.write(@balance.round(2))}
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
--- hometask13_12/txtcrud.rb
+++ hometask13_12/txtcrud.rb
@@ -1,58 +1,58 @@
 def index(path)
-    File.foreach(path) { |line| puts line }
+  File.foreach(path) { |line| puts line }
 end
 
 def find(path, id)
-    res = nil
-    File.foreach(path).with_index do |line, index|
-        if index == id
-            res = line
-            break
-        end
+  res = nil
+  File.foreach(path).with_index do |line, index|
+    if index == id
+      res = line
+      break
     end
-    unless res.nil?
-        res.chomp
-    else
-        res
-    end
+  end
+  unless res.nil?
+    res.chomp
+  else
+    res
+  end
 end
 
 def where(path, pattern)
-    list = []
-    File.foreach(path).with_index do |line, index|
-        if line.include?(pattern)
-            list.push(index)
-        end
+  list = []
+  File.foreach(path).with_index do |line, index|
+    if line.include?(pattern)
+      list.push(index)
     end
-    list
+  end
+  list
 end
 
 def update(path, id, text)
-    buffer = 'buffer.txt'
-    file = File.open(buffer, 'w')
-    File.foreach(path).with_index do |line, index|
-        file.puts(id == index ? text : line)
-    end
-    file.close
-    File.write(path, File.read(buffer))
-    File.delete(buffer) if File.exist?(buffer)
+  buffer = 'buffer.txt'
+  file = File.open(buffer, 'w')
+  File.foreach(path).with_index do |line, index|
+    file.puts(id == index ? text : line)
+  end
+  file.close
+  File.write(path, File.read(buffer))
+  File.delete(buffer) if File.exist?(buffer)
 end
 
 def delete(path, id)
-    buffer = 'buffer.txt'
-    file = File.open(buffer, 'w')
-    File.foreach(path).with_index do |line, index|
-        if index != id
-            file.puts(line)
-        end
+  buffer = 'buffer.txt'
+  file = File.open(buffer, 'w')
+  File.foreach(path).with_index do |line, index|
+    if index != id
+      file.puts(line)
     end
-    file.close
-    File.write(path, File.read(buffer))
-    File.delete(buffer) if File.exist?(buffer)
+  end
+  file.close
+  File.write(path, File.read(buffer))
+  File.delete(buffer) if File.exist?(buffer)
 end
 
 def create(path, text)
-    file = File.open(path, 'a')
-    file.puts(text)
-    file.close
+  file = File.open(path, 'a')
+  file.puts(text)
+  file.close
 end
