8f50e5b7e45002bb6ee5f80f9ee969e7e70b8012
maxporyvay <sevmaxclever@gmail.com>
2022-12-20 01:07:11 +0300

    Minor indent changes in 08.12 hometask

--- hometask08_12/posts_controller.rb
+++ hometask08_12/posts_controller.rb
@@ -1,62 +1,62 @@
 require_relative "resource"
 
 class PostsController
-    extend Resource
+  extend Resource
   
-    def initialize
-        @posts = []
+  def initialize
+    @posts = []
+  end
+  
+  def index
+    str = ""
+    @posts.each.with_index do |post, index|
+      str += "#{index}. #{post}\n"
     end
+    print str
+  end
   
-    def index
-        str = ""
-        @posts.each.with_index do |post, index|
-            str += "#{index}. #{post}\n"
-        end
-        print str
-    end
-  
-    def show
-        print "Enter post id>"
-        id = gets.chomp.to_i
+  def show
+    print "Enter post id>"
+    id = gets.chomp.to_i
 
-        if id < 0 || id >= @posts.size
-            puts "ERROR! There is no post with this id"
-        else
-            puts "#{id}. #{@posts[id]}"
-        end
+    if id < 0 || id >= @posts.size
+      puts "ERROR! There is no post with this id"
+    else
+      puts "#{id}. #{@posts[id]}"
     end
+  end
   
-    def create
-        print "Enter post text>"
-        newtext = gets.chomp
+  def create
+    print "Enter post text>"
+    newtext = gets.chomp
 
-        id = @posts.size
-        @posts << newtext
-        puts "#{id}. #{newtext}"
-    end
+    id = @posts.size
+    @posts << newtext
+    puts "#{id}. #{newtext}"
+  end
   
-    def update
-        print "Enter post id>"
-        id = gets.chomp.to_i
-        print "Enter post text>"
-        newtext = gets.chomp
+  def update
+    print "Enter post id>"
+    id = gets.chomp.to_i
+    print "Enter post text>"
+    newtext = gets.chomp
 
-        if id < 0 || id >= @posts.size
-            puts "ERROR! There is no post with this id"
-        else
-            @posts[id] = newtext
-            puts "#{id}. #{newtext}"
-        end
+    if id < 0 || id >= @posts.size
+      puts "ERROR! There is no post with this id"
+    else
+      @posts[id] = newtext
+      puts "#{id}. #{newtext}"
     end
+  end
   
-    def destroy
-        print "Enter post id>"
-        id = gets.chomp.to_i
+  def destroy
+    print "Enter post id>"
+    id = gets.chomp.to_i
 
-        if id < 0 || id >= @posts.size
-            puts "ERROR! There is no post with this id"
-        else
-            @posts.delete_at(id)
-        end
+    if id < 0 || id >= @posts.size
+      puts "ERROR! There is no post with this id"
+    else
+      @posts.delete_at(id)
     end
+  end
 end
